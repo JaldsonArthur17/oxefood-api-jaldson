1724f0745bebe43d6f6a40e3b3c879c6d0108315
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-09-03 19:13:56 -0300

    refactor: corrigindo nome errado na tabela

--- src/main/java/br/edu/ifpe/oxefood/api/empresa/Empresa.java
+++ src/main/java/br/edu/ifpe/oxefood/api/empresa/Empresa.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "oxefood_jaldson")
+@Table(name = "empresa")
 public class Empresa {
 
     @Id
--- src/main/java/br/edu/ifpe/oxefood/api/produto/Produto.java
+++ src/main/java/br/edu/ifpe/oxefood/api/produto/Produto.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "oxefood_produto")
+@Table(name = "produto")
 public class Produto {
 
     @Id
